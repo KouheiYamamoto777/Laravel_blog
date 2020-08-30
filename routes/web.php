a3b44febea7fcf50add89415b0980396ffe44b2e
KouheiYamamoto777 <kouheiyamamoto777@gmail.com>
2020-08-30 19:20:04 +0900

    ブログ編集に関するルーティングを記述しました。

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,9 @@ Route::post('/blog/store', 'BlogController@exeStore')->name('store');
 
 // ブログ詳細画面を表示する
 Route::get('/blog/{id}', 'BlogController@showDetail')->name('show');
+
+// ブログ編集画面を表示する
+Route::get('/blog/edit/{id}', 'BlogController@showEdit')->name('edit');
+
+// ブログを編集する
+Route::post('/blog/update', 'BlogController@exeUpdate')->name('update');
