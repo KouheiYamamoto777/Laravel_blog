c92f2a8cd382840acd706a7728ec370f134d10cd
KouheiYamamoto777 <kouheiyamamoto777@gmail.com>
2020-08-30 16:25:12 +0900

    factoryとseederの内容を追記しました。

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -2,11 +2,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
+use App\Models\Blog;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Blog::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->word,
+        'content' => $faker->realText
     ];
 });
--- database/seeds/BlogsTableSeeder.php
+++ database/seeds/BlogsTableSeeder.php
@@ -12,6 +12,6 @@ class BlogsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(Blog::class, 15)->create();
     }
 }
